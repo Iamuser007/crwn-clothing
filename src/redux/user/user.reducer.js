b78537f1fd53ed6881786d6256c9ca8881fbd9f3
Jasper LO <mufasaleostein@gmail.com>
2023-11-02 05:29:22 +0100

    just added redux

---
+++ src/redux/user/user.reducer.js
@@ -0,0 +1,20 @@
+// * we need to be specific about the type of action
+
+const INITIAL_STATE = {
+  currentUser: null,
+};
+
+const userReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case "SET_CURRENT_USER":
+      return {
+        ...state,
+        currentUser: action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default userReducer;
